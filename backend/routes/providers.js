c1e90b58adcdf7ccd25a6872b5e41b26e7e3c78e
arriolaleo <riblarsv@gmail.com>
2021-11-25 14:39:56 +0600

    crud for providers create is need to finish swagger

--- backend/routes/providers.js
+++ backend/routes/providers.js
@@ -7,10 +7,22 @@ function providersAPI(app) {
 
   router.get("/", async (req, res, next) => {
     try {
-      const providers = await providerService.getProviders();
+      const providers = await providerService.getAll();
       res.status(200).json({
         data: providers,
-        message: "retriving all the providers",
+        message: "providers retrived",
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+  router.get("/:idProvider", async (req, res, next) => {
+    const { idProvider } = req.params;
+    try {
+      const provider = await providerService.getOne(idProvider);
+      res.status(200).json({
+        data: provider,
+        message: `${idProvider}  fetched`,
       });
     } catch (err) {
       next(err);
@@ -20,10 +32,37 @@ function providersAPI(app) {
   router.post("/", async (req, res, next) => {
     const { body: data } = req;
     try {
-      const newProvider = providerService.createProvider(data);
+      const newProvider = providerService.create(data);
       res.status(201).json({
         data: newProvider,
-        message: "new provided created",
+        message: "created",
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.patch("/:idProvider", async (req, res, next) => {
+    const { body: data } = req;
+    const { idProvider } = req.paramsa;
+    try {
+      const updateProviders = providerService.update(id, data);
+      res.status(200).json({
+        data: updateProvider,
+        message: "updated",
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.delete("/:idProvider", async (req, res, next) => {
+    const { idProvider } = req.params;
+    try {
+      const providerDeleted = await providerService.delete();
+      res.status(200).json({
+        data: providerDeleted,
+        message: "deleted",
       });
     } catch (err) {
       next(err);
